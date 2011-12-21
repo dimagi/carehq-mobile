468bcbd9bef654549f179cedfa73c9d53048731a
Christian Ternus <ternus@mit.edu>
2011-12-21 16:05:33 -0500

    update view with api key

--- carehq_mobile/urls.py
+++ carehq_mobile/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('',
-    url(r'^/$', 'carehq_mobile.views.carehq_mobile', name='carehq_mobile'),
+    url(r'^$', 'carehq_mobile.views.carehq_mobile', name='carehq_mobile'),
 
 )
--- carehq_mobile/views.py
+++ carehq_mobile/views.py
@@ -1,8 +1,15 @@
 # Create your views here.
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
 @login_required
 def carehq_mobile(request, template='carehq_mobile/shine-ui.html'):
-    return render_to_response(template, {}, context_instance=RequestContext(request))
+    if not request.user:
+        return HttpResponseRedirect("/")
+    if not request.user.api_key:
+        return Http404()
+    context = dict()
+    context['key'] = request.user.api_key.key
+    return render_to_response(template, context, context_instance=RequestContext(request))
